b422fe13ae8e1a4575683c5ffa6eeeed94dd22b8
Sam Collinson <sam@6sync.com>
2013-09-17 18:34:16 +1200

    add WinDriver includes

--- include/luxrays/luxrays.h
+++ include/luxrays/luxrays.h
@@ -26,6 +26,11 @@
 
 #include "luxrays/cfg.h"
 
+#include "wdc_defs.h"
+#include "wdc_lib.h"
+#include "driver_lib.h"
+#define DRIVER_ERR printf
+
 //------------------------------------------------------------------------------
 // Configure unicode support (requires Boost version 1.50 or better)
 
